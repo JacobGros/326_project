5a4c6d506f645533c2808c6be08eb7a47ff7ea4e
vagrant <vagrant@vagrant.vm>
2018-10-29 19:05:13 +0000

    first draft of pet model

--- src/petsProject/PUPG/models.py
+++ src/petsProject/PUPG/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+
+    name = models.CharField(max_length=20, help_text='Enter Name')
+    animal_type = models.CharField(max_length=20, help_text='Enter Animal Type')
+    animal_breed = models.CharField(max_length=20, help_text='Enter Breed')
+    pet_id = models.IntegerField()
+    vote_count = models.IntegerField()
+
+
