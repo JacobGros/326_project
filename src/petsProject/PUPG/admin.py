ccc2622efd143880ffbb07d44037399fb0c6e9c3
vagrant <vagrant@vagrant.vm>
2018-10-30 17:22:16 +0000

    Updated models.py and admin.py

--- src/petsProject/PUPG/admin.py
+++ src/petsProject/PUPG/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from PUPG.models import User, Pet
+
+admin.site.register(User)
+admin.site.register(Pet)
--- src/petsProject/PUPG/models.py
+++ src/petsProject/PUPG/models.py
@@ -6,7 +6,16 @@ class Pet(models.Model):
     name = models.CharField(max_length=20, help_text='Enter Name')
     animal_type = models.CharField(max_length=20, help_text='Enter Animal Type')
     animal_breed = models.CharField(max_length=20, help_text='Enter Breed')
-    pet_id = models.IntegerField()
+    pid = models.IntegerField()
     vote_count = models.IntegerField()
+    pet_owner = models.CharField(max_length=20, help_text='Enter Owner ID')
+    age = models.IntegerField()
+    vote_count  = models.IntegerField()
 
-
+class User(models.Model):
+    name = models.CharField(max_length=20, help_text='Enter Name')
+    User_id = models.IntegerField()
+    pet_id = models.IntegerField()
+    username = models.CharField(max_length=20, help_text='Enter Username')
+    age = models.IntegerField()
+    country = models.CharField(max_length=20, help_text='Enter Country')
