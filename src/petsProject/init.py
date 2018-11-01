b4757a3c50fb1221432b193b73950db3a77dfc91
jcapozzi2 <43500113+jcapozzi2@users.noreply.github.com>
2018-10-31 22:05:56 -0400

    Template
    Initial template for the init.py function which will add model data later...

---
+++ src/petsProject/init.py
@@ -0,0 +1,74 @@
+import os
+
+def populate():
+    python_pet = add_pet('Python')
+
+    add_pet(pet=python_pet,
+        name="Fred",
+        animal_breed="Dog"
+        pid="4"
+        vote_count="24"
+        age="2"
+        )
+
+    add_page(pet=python_pet,
+        name="Bob",
+        animal_breed="Dog"
+        pid="5"
+        vote_count="26"
+        age="1"
+        )
+
+    add_page(pet=python_pet,
+        name="Randy",
+        animal_breed="Cat"
+        pid="2"
+        vote_count="20"
+        age="3"
+        )
+
+    django_pet = add_pet("Django")
+
+    add_pet(pet=django_pet,
+        name="Randy",
+        animal_breed="Cat"
+        pid="2"
+        vote_count="20"
+        age="3"
+        )
+
+    add_page(cat=django_cat,
+        name="Bob",
+        animal_breed="Dog"
+        pid="5"
+        vote_count="26"
+        age="1"
+        )
+
+    add_page(cat=django_cat,
+        name="Fred",
+        animal_breed="Dog"
+        pid="4"
+        vote_count="24"
+        age="2"
+        )
+
+    # Print out what we have added to the user.
+    for c in Category.objects.all():
+        for p in Pet.objects.filter(category=c):
+            print "- {0} - {1}".format(str(c), str(p))
+
+def add_pet(pup, animal_breed, pid, vote_count, age):
+    p = Pet.objects.get_or_create(category=pup, animal_breed=animal_breed, pid=pid, vote_count=vote_count)[0]
+    return p
+
+def add_pup(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    return c
+
+# Start execution here!
+if __name__ == '__main__':
+    print "Starting Rango population script..."
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+    from rango.models import Category, Pet
+    populate()
