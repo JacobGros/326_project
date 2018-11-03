4108d796fe7f4735eee5ad5d1d840b7c13ce09ff
vagrant <vagrant@vagrant.vm>
2018-11-03 16:42:48 +0000

    update init.py

--- src/petsProject/init.py
+++ src/petsProject/init.py
@@ -1,52 +1,44 @@
 import os
 
 def populate():
-    python_pet = add_pet('Python')
+    
 
-    add_pet(pet=python_pet,
-        name="Fred",
+    add_pet(name="Fred",
         animal_breed="Dog",
         pid= 4,
         vote_count= 24,
         age= 2
         )
 
-    add_pet(pet=python_pet,
-        name="Bob",
+    add_pet(name="Bob",
         animal_breed="Dog",
         pid= 5,
         vote_count= 26,
         age= 1
         )
 
-    add_pet(pet=python_pet,
-        name="Randy",
+    add_pet(name="Randy",
         animal_breed="Cat",
         pid= 2,
         vote_count= 20,
         age= 3
         )
 
-    django_pet = add_pet("Django")
-
-    add_pet(pet=django_pet,
-        name="Randy",
+    add_pet(name="Randy",
         animal_breed="Cat",
         pid= 2,
         vote_count= 20,
         age= 3
         )
 
-    add_pet(cat=django_cat,
-        name="Bob",
+    add_pet(name="Bob",
         animal_breed="Dog",
         pid= 5,
         vote_count= 26,
         age= 1
         )
 
-    add_pet(cat=django_cat,
-        name="Fred",
+    add_pet(name="Fred",
         animal_breed="Dog",
         pid= 4,
         vote_count= 24,
@@ -54,17 +46,17 @@ def populate():
         )
 
     # Print out what we have added to the user.
-    for c in Category.objects.all():
+    for c in User.objects.all():
         #print(c) 
-        for p in Pet.objects.filter(category==c):
+        for p in Pet.objects.filter(user==c):
            print( "- {0} - {1}".format(str(c), str(p)))
 
-def add_pet(pup, animal_breed, pid, vote_count, age):
-    p = Pet.objects.get_or_create(category=pup, animal_breed=animal_breed, pid=pid, vote_count=vote_count)[0]
+def add_pet(name, animal_type, animal_breed, pid, vote_count, pet_owner, age):
+    p = Pet.objects.get_or_create(name=name, animal_type=animal_type, animal_breed=animal_breed, pid=pid, vote_count=vote_count, pet_owner=pet_owner, age=age)[0]
     return p
 
-def add_pup(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_user(name, User_id, pet_id, username, age, country):
+    c = User.objects.get_or_create(name=name, User_id=User_id, pet_id=pet_id, username=username, age=age, country=country)[0]
     return c
 
 # Start execution here!
