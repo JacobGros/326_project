edb920199d8377623856494358a89d647800f161
Jacob <vagrant@vagrant.vm>
2018-11-04 03:06:58 +0000

    some changes

--- src/petsProject/PUPG/models.py
+++ src/petsProject/PUPG/models.py
@@ -8,9 +8,13 @@ class Pet(models.Model):
     animal_breed = models.CharField(max_length=20, help_text='Enter Breed')
     pid = models.IntegerField()
     vote_count = models.IntegerField()
-    pet_owner = models.CharField(max_length=20, help_text='Enter Owner ID')
+    pet_owner = models.ForeignKey('User', on_delete=models.SET_NULL, null = True)
+    #pet_owner = models.CharField(max_length=20, help_text='Enter Owner ID')
     age = models.IntegerField()
-    
+    #image = models.ImageField(upload_to = this.pid) 
+
+
+
     def __str__(self):
         return "Name:  "+ self.name + "     Species: " + self.animal_type + "     Vote Count: " + str(self.vote_count)
     
@@ -20,5 +24,10 @@ class User(models.Model):
     User_id = models.IntegerField()
     pet_id = models.IntegerField()
     username = models.CharField(max_length=20, help_text='Enter Username')
+    password = models.CharField(max_length=20, help_text='Enter Password')
     age = models.IntegerField()
     country = models.CharField(max_length=20, help_text='Enter Country')
+
+
+    def __str__(self):
+        return "Name: " + self.name + " Username: " + self.username + " Password: " + self.password
