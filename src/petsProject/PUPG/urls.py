f7c3a5bfe45b7b3fe7c61fd987d85e509c147245
vagrant <vagrant@vagrant.vm>
2018-11-05 23:11:10 +0000

    updated viwes.py urls.py and generic template

--- src/petsProject/PUPG/urls.py
+++ src/petsProject/PUPG/urls.py
@@ -4,5 +4,7 @@ from PUPG import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("pets/", views.petsview, name="pets"),
+    path("pets/", views.petsview.as_view(), name="pets"),
+    path("submit/", views.submit, name="submit"),
+    path("leaderboard/", views.leaderboard, name="leaderboard"),
 ]
--- src/petsProject/PUPG/views.py
+++ src/petsProject/PUPG/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from PUPG.models import Person, Pet
+from django.views import generic
 # Create your views here.
 
 def index(request):
 
      num_pets = Pet.objects.all().count()
-     num_people = Person.objects.all()count()
+     num_people = Person.objects.all().count()
 
 
      context = {
@@ -14,3 +16,23 @@ def index(request):
             }
 
      return render(request, "index.html", context=context)
+
+
+
+def leaderboard(request):
+    num_pets = Pet.objects.all().count()
+    return render(request, "index.html", context=context)
+    context = {
+            "num_pets": num_pets,
+            }
+
+def submit(request):
+    num_pets = Pet.objects.all().count()
+    return render(request, "index.html", context=context)
+    conext = {
+            "num_pets": num_pets,
+            }
+
+class petsview(generic.ListView):
+    model = Pet
+    
