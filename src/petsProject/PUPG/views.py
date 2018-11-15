9a02177e956a79350e33f8ace2c431766784c689
jcapozzi2 <43500113+jcapozzi2@users.noreply.github.com>
2018-11-14 19:16:24 -0500

    edit line 24

--- src/petsProject/PUPG/views.py
+++ src/petsProject/PUPG/views.py
@@ -20,8 +20,8 @@ def index(request):
     context = {
             "num_pets": num_pets,
             "num_people": num_people,
-            "winner":ordered_pets[0]
-            " ":ordered_pets[random.randint(0,200)]
+            "winner": ordered_pets[0]
+            "random pet": ordered_pets[random.randint(0,200)]
             }
 
     return render(request, "index.html", context=context)
