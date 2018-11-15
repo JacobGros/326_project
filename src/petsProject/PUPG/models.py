d006e452328362fe400c1f0d38ef06af008b7414
Jacob <vagrant@vagrant.vm>
2018-11-15 18:44:52 +0000

    removed useless model

--- src/petsProject/PUPG/models.py
+++ src/petsProject/PUPG/models.py
@@ -22,23 +22,6 @@ class Pet(models.Model):
     def __str__(self):
         return "Name:  "+ self.name +  "     Picture: " + self.picture.name + "     Species: " + self.animal_type + "     Vote Count: " + str(self.vote_count)
     
-#this class isn't used anywhere, it's existence is a mistake. 
-class User(models.Model):
-    name = models.CharField(max_length=20, help_text='Enter Name')
-    User_id = models.IntegerField()
-    pet_id = models.IntegerField()
-    username = models.CharField(max_length=20, help_text='Enter Username')
-    password = models.CharField(max_length=20, help_text='Enter Password')
-    age = models.IntegerField()
-    country = models.CharField(max_length=20, help_text='Enter Country')
-
-
-    def __str__(self):
-        return "Name: " + self.name + " Username: " + self.username + " Password: " + self.password
-
-
-
-
 
 class Person(models.Model):
     name = models.CharField(max_length=20, help_text='Enter Name')
