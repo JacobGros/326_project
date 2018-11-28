dde853d1687e0abec822c1d44242b4dd8cf2e878
Jacob <vagrant@vagrant.vm>
2018-11-28 23:41:37 +0000

    fixed some things

--- src/petsProject/PUPG/models.py
+++ src/petsProject/PUPG/models.py
@@ -10,11 +10,8 @@ class Pet(models.Model):
 
     name = models.CharField(max_length=20, help_text='Enter Name')
     animal_type = models.CharField(max_length=20, help_text='Enter Animal Type')
-    #animal_breed = models.CharField(max_length=20, help_text='Enter Breed')
-    pid = models.IntegerField()
     vote_count = models.IntegerField()
     pet_owner = models.ForeignKey('Person', on_delete=models.SET_NULL, null = True)
-    #pet_owner = models.CharField(max_length=20, help_text='Enter Owner ID')
     age = models.IntegerField()
     picture = models.ImageField(max_length = 255) 
     
@@ -29,9 +26,6 @@ class Pet(models.Model):
 class Person(models.Model):
     name = models.CharField(max_length=20, help_text='Enter Name')
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
-    #username = models.CharField(max_length=20, help_text='Enter Username')
-    #password = models.CharField(max_length=20, help_text='Enter Password')
-    #age = models.IntegerField(null=True, blank=True)
     country = models.CharField(max_length=20, help_text='Enter Country')
    
     @receiver(post_save, sender=User)
@@ -39,16 +33,6 @@ class Person(models.Model):
         if created:
             Person.objects.create(user=instance)
         instance.person.save()
-
-    #@receiver(post_save, sender=User)
-    #def create_user_person(sender, instance, created, **kwargs):
-    #    if created:
-    #        Person.objects.create(user=instance)
-
-    #@receiver(post_save, sender=User)
-    #def save_user_person(sender, instance, **kwargs):
-    #    instance.person.save()
-
 
     def __str__(self):
         return "Name: " + self.name + " Username: " + self.user.username + " Password: " + self.user.password
