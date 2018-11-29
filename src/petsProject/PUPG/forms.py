b616c527a801b4ab4094da24a08ebc717aba8e9e
PatrickRobb <vagrant@vagrant.vm>
2018-11-29 19:09:45 +0000

    added country form box in registration which assigns a country to the person associated with the user

--- src/petsProject/PUPG/forms.py
+++ src/petsProject/PUPG/forms.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    country = forms.CharField(max_length=30, required=False, help_text='Optional.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'country', 'password1', 'password2', )
 
 #picture  = forms.ImageField(label = 'Upload your cute pet pics!',
  #                                         help_text = 'The image should be cute.')
